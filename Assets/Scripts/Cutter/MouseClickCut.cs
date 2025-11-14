f45593b98cd0a17b9e6275944009ac39d9c760da
ArmandGemeux <armandgemeux@hotmail.fr>
2025-11-14 16:56:55 +0100

    Slicer addition

---
+++ Assets/Scripts/Cutter/MouseClickCut.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Angle
+{
+    Right,
+    Up,
+    Forward
+}
+public class MouseClickCut : MonoBehaviour
+{
+    public Angle angle;
+
+    void Update(){
+
+		if(Input.GetMouseButtonDown(0)){
+			RaycastHit hit;
+
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)){
+
+				GameObject victim = hit.collider.gameObject;
+				if(victim.tag == "Cuttable")
+				{
+                   
+                    if(angle == Angle.Right)
+					{
+                        Cutter.Cut(victim, hit.point, new Vector3(0.01f,0f,0f));
+
+                    }
+                    else if (angle == Angle.Up)
+                    {
+                        Cutter.Cut(victim, hit.point, Vector3.up);
+
+                    }
+                    else if (angle == Angle.Forward)
+					{
+						Cutter.Cut(victim, hit.point, Vector3.forward);
+						
+					}
+				}
+			}
+
+		}
+	}
+}
